79aa2441e48fbf5f3c317c0b30204d7cf94b3a5e
fernandamackias <115710123+fernandamackias@users.noreply.github.com>
2023-04-20 08:06:58 -0300

    Add files via upload

---
+++ men.js
@@ -0,0 +1,10 @@
+const toggleBtn = document.getElementById('toggle-mode');
+const body = document.body;
+const header = document.getElementById('head')
+
+toggleBtn.addEventListener("click", function() {
+    body.classList.toggle("dark-mode")
+    header.classList.toggle("dark-mode-header")
+}
+)
+
